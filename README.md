45f8d62e1e32fe6af64f898a3c474d784b052bc6
bagoes-wahnu <bagoeswn@gmail.com>
2023-01-16 11:54:07 +0700

    ss

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # bsy
 
+## Halaman Login
+![Alt text](/screenshot_bsy/login.jpg)
+
+## Halaman Dashboard
+![Alt text](/screenshot_bsy/dashboard.jpg)
+
+## Halaman Info Tabungan
+![Alt text](/screenshot_bsy/info_tabungan1.jpg)
+![Alt text](/screenshot_bsy/info_tabungan2.jpg)
+
+## Halaman Upload Target Keuangan
+![Alt text](/screenshot_bsy/upload_target_keuangan1.jpg)
+![Alt text](/screenshot_bsy/upload_target_keuangan2.jpg)
+
+## Halaman Upload Data Realisasi
+![Alt text](/screenshot_bsy/data_realisasi.png)
+![Alt text](/screenshot_bsy/data_realisasi2.png)
+![Alt text](/screenshot_bsy/data_realisasi3.png)
+
+<!-- ## Halaman Report
+![Alt text](/screenshot_penomoran/report.png)
+
+## Halaman Report Preview
+![Alt text](/screenshot_penomoran/report_preview.png) -->
--- application/config/database.php
+++ application/config/database.php
@@ -95,12 +95,30 @@ $query_builder = TRUE;
 // 	'save_queries' => TRUE
 // );
 
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
+
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '103.93.59.233',
-	'username' => 'demo',
-	'password' => '3n3rg33kDB!@#',
-	'database' => 'demo_ibsy',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'document_bsy',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
